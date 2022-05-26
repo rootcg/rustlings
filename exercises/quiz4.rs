44b881b883aa9d2d6d909f096f7e4da76e7154d8
Cristian González Morante <cristian.gonzalez.morante@protonmail.com>
2022-05-26 10:22:51 +0000

    quiz4 done

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,19 +5,30 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+mod my_macro_mod {
+    #[macro_export]
+    macro_rules! custom_macro {
+        ( $x:expr) => {
+            {
+                let mut str: String = String::from("Hello ");
+                str.push_str($x);
+                str
+            }
+        };
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::custom_macro;
 
     #[test]
     fn test_my_macro_world() {
-        assert_eq!(my_macro!("world!"), "Hello world!");
+        assert_eq!(custom_macro!("world!"), "Hello world!");
     }
 
     #[test]
     fn test_my_macro_goodbye() {
-        assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
+        assert_eq!(custom_macro!("goodbye!"), "Hello goodbye!");
     }
 }
